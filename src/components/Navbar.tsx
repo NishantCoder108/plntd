ef9b4eba97fdac0f9a6b8e01976c8142eb7af0a2
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-03-12 20:32:36 +0530

    add logo and ui improve of navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,90 +1,34 @@
 "use client";
-import { PROTOCOL_CONFIG } from "@/constants/protocol";
-// import {
-//   DropdownMenu,
-//   DropdownMenuContent,
-//   DropdownMenuItem,
-//   DropdownMenuTrigger,
-// } from "./ui/dropdown-menu";
-// import { Button } from "./ui/button";
-// import { ChevronDown, Shield } from "lucide-react";
-// import { useWallet } from "@solana/wallet-adapter-react";
+import logoImg from "/public/logo.svg";
+
 import {
-  //   BaseWalletMultiButton,
-  //   WalletDisconnectButton,
+  BaseWalletMultiButton,
   WalletModalProvider,
-  WalletMultiButton,
 } from "@solana/wallet-adapter-react-ui";
-// import { useSocket } from "@/contexts/socketContext";
-// import toast from "react-hot-toast";
-// import { WALLET_LABELS } from "@/constants/wallet";
+import Image from "next/image";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WALLET_LABELS } from "@/constants/wallet";
 
 const Navbar = () => {
-  //   const { publicKey, connected, disconnect } = useWallet();
-  //   const socket = useSocket();
-  //   const wallet = useWallet();
-  //   const isWalletConnected = wallet.connected;
+  const { connected } = useWallet();
 
-  //   const connectedStyle = {
-  //     border: "1px solid #9333ea",
-  //     background: "#9333ea",
-  //     borderRadius: "1rem",
-  //     height: "2.3rem",
-  //   };
-  //   const disconnectedStyle = {
-  //     border: "1px solid #9333ea",
-  //     background: "#9333ea",
-  //     borderRadius: "1rem",
-  //     height: "2.3rem",
-  //     fontSize: "0.9rem",
-  //     fontFamily: "inherit",
-  //   };
-  //   const disconnectWallet = () => {
-  //     disconnect();
-  //     toast.success("Wallet disconnected successfully");
-
-  //     if (socket) {
-  //       socket.disconnect();
-  //     }
-  //   };
   return (
     <>
-      <div className="flex flex-col md:flex-row justify-between items-center mb-8">
+      <div className="flex flex-col md:py-4  md:flex-row justify-between  items-center mb-8">
         <div>
-          <h1 className="text-4xl font-bold mb-2 bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-green-950">
-            {PROTOCOL_CONFIG.NAME}
-          </h1>
-          <p className="text-gray-400">Eco-friendly crypto staking platform</p>
+          <div>
+            <Image src={logoImg} title="plntd logo" alt="logo" width={55} />
+          </div>
         </div>
-        <div className="flex flex-col items-end gap-2">
-          <>
-            {/* <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="secondary" className="flex items-center gap-2">
-                  <Shield className="h-4 w-4" />
-                  {publicKey?.toBase58().slice(0, 4) +
-                    "..." +
-                    publicKey?.toBase58().slice(-4)}
-                  <ChevronDown className="h-4 w-4" />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end" className="">
-                <DropdownMenuItem
-                  onClick={disconnectWallet}
-                  className="cursor-pointer "
-                >
-                  Disconnect
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu> */}
-
-            <WalletModalProvider>
-              <WalletMultiButton
-                style={{ background: "transparent", color: "green" }}
+        <div className="flex items-center gap-2">
+          <WalletModalProvider>
+            <div className="flex items-center justify-between">
+              <BaseWalletMultiButton
+                style={connected ? connectedStyle : disconnectedStyle}
+                labels={WALLET_LABELS}
               />
-              {/* <WalletDisconnectButton /> */}
-            </WalletModalProvider>
-          </>
+            </div>
+          </WalletModalProvider>
         </div>
       </div>
     </>
@@ -92,3 +36,25 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+const connectedStyle = {
+  height: "2.5rem",
+  width: "10rem",
+  fontSize: ".875rem",
+  borderRadius: "2rem",
+  fontFamily: "var(--font-roboto)",
+  cursor: "pointer",
+  background: "#A3DE83",
+  color: "#1E1E1E",
+};
+const disconnectedStyle = {
+  height: "2.5rem",
+  width: "10rem",
+  fontSize: ".875rem",
+  border: ".5px solid gray",
+  fontFamily: "var(--font-roboto)",
+  background: "#A3DE83",
+  color: "#1E1E1E",
+  borderRadius: "2rem",
+  cursor: "pointer",
+};
