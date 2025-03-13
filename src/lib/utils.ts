b59378b392da0ef207c56eb6df33022531eee03d
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-03-13 16:41:50 +0530

    utils for format string

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatString = (str: string, start: number, end: number) => {
+  return str.slice(0, start) + "..." + str.slice(-end);
+};
+
+export const copyToClipboard = (text: string) => {
+  navigator.clipboard.writeText(text);
+};
