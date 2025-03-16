84d0e5be806c1eb507954c94e0dcb70c18ffc3be
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-03-16 15:44:55 +0530

    interactive with actual data showing implement

--- src/components/trading/TradeToken.tsx
+++ src/components/trading/TradeToken.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import TradeCard from "./TradeCard";
 import sol_token from "/public/sol_token.svg";
@@ -8,24 +8,84 @@ import plntd_token from "/public/plntd_token.svg";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import toast from "react-hot-toast";
 import {
+  Connection,
   LAMPORTS_PER_SOL,
   PublicKey,
   SystemProgram,
   Transaction,
 } from "@solana/web3.js";
-import { MINTER_PUBLIC_KEY, TOKEN_MINT_ADDRESS } from "@/constants/wallet";
 import {
+  MINTER_PUBLIC_KEY,
+  PLNTD_SOL_ADDRESS,
+  RPC_URL,
+  TOKEN_MINT_ADDRESS,
+} from "@/constants/wallet";
+import {
+  createTransferInstruction,
+  getAccount,
   getAssociatedTokenAddress,
   TOKEN_2022_PROGRAM_ID,
 } from "@solana/spl-token";
+import { formatString } from "@/lib/utils";
 
 export default function TradeToken() {
   const [activeTab, setActiveTab] = useState<"buy" | "sell">("buy");
   const [solAmount, setSolAmount] = useState<string>("1");
-  const { publicKey, sendTransaction } = useWallet();
+  const [plntdAmount, setPlntdAmount] = useState<string>("0.5");
+  const wallet = useWallet();
+  const { publicKey, sendTransaction } = wallet;
   const { connection } = useConnection();
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [plntdBalance, setPlntdBalance] = useState<number>(0);
+  const [solBalance, setSolBalance] = useState<number>(0);
 
+  const getStakedTokenBalance = async (
+    walletAddress: PublicKey
+  ): Promise<number> => {
+    try {
+      const conn = new Connection(RPC_URL);
+      const walletPubkey = walletAddress;
+      const tokenMintPubkey = new PublicKey(TOKEN_MINT_ADDRESS);
+
+      const associatedTokenAddress = await getAssociatedTokenAddress(
+        tokenMintPubkey,
+        walletPubkey,
+        false,
+        TOKEN_2022_PROGRAM_ID
+      );
+
+      const accountInfo = await getAccount(
+        conn,
+        associatedTokenAddress,
+        "confirmed",
+        TOKEN_2022_PROGRAM_ID
+      );
+
+      console.log({ accountInfo });
+      setPlntdBalance(Number(accountInfo.amount));
+
+      return Number(accountInfo.amount);
+    } catch (error) {
+      console.log({ error });
+
+      return 0;
+    }
+  };
+
+  const fetchUserBalance = async (
+    walletAddress: PublicKey
+  ): Promise<number> => {
+    try {
+      const conn = new Connection(RPC_URL);
+      const balance = await conn.getBalance(walletAddress);
+      setSolBalance(balance / LAMPORTS_PER_SOL);
+      return balance / LAMPORTS_PER_SOL;
+    } catch (error) {
+      console.log({ error });
+      setSolBalance(0);
+      return 0;
+    }
+  };
   const handleTrade = async (type: "buy" | "sell") => {
     if (!publicKey) {
       toast.error("Please connect your wallet to continue");
@@ -49,7 +109,7 @@ export default function TradeToken() {
       const transaction = new Transaction().add(
         SystemProgram.transfer({
           fromPubkey: publicKey,
-          toPubkey: associatedTokenAdd,
+          toPubkey: new PublicKey(PLNTD_SOL_ADDRESS),
           lamports: Number(solAmount) * LAMPORTS_PER_SOL,
         })
       );
@@ -72,18 +132,121 @@ export default function TradeToken() {
 
       toast.success("Tokens Purchased!");
       setIsLoading(false);
+      getStakedTokenBalance(publicKey);
+      fetchUserBalance(publicKey);
     } catch (error) {
       toast.error("Something went wrong. Please try again");
       console.log(error);
       setIsLoading(false);
     }
   };
+
+  const ataExists = async (connection: Connection, ataAddress: PublicKey) => {
+    try {
+      const account = await getAccount(
+        connection,
+        ataAddress,
+        "confirmed",
+        TOKEN_2022_PROGRAM_ID
+      );
+      console.log({ account });
+      return true;
+    } catch {
+      return false;
+    }
+  };
+
+  const handleSell = async () => {
+    if (!publicKey) {
+      toast.error("Please connect your wallet to continue");
+      return;
+    }
+    setIsLoading(true);
+    try {
+      const walletPubKey = new PublicKey(MINTER_PUBLIC_KEY);
+      const tokenMint = new PublicKey(TOKEN_MINT_ADDRESS);
+
+      const senderATA = await getAssociatedTokenAddress(
+        tokenMint,
+        publicKey,
+        false,
+        TOKEN_2022_PROGRAM_ID
+      );
+
+      console.log("senderATA address 01: ", senderATA.toString());
+
+      if (!ataExists(connection, senderATA)) {
+        toast.error("Sender ATA does not exist");
+        return;
+      }
+
+      const recipientATA = await getAssociatedTokenAddress(
+        tokenMint,
+        walletPubKey,
+        false,
+        TOKEN_2022_PROGRAM_ID
+      );
+
+      console.log("recipientATA address : ", recipientATA.toString());
+
+      const transferIx = createTransferInstruction(
+        senderATA,
+        recipientATA,
+        publicKey,
+        Number(plntdAmount) * 1000000, // This will convert 0.5 to 500000
+        [publicKey],
+        TOKEN_2022_PROGRAM_ID
+      );
+
+      const transaction = new Transaction().add(transferIx);
+
+      const { blockhash, lastValidBlockHeight } =
+        await connection.getLatestBlockhash();
+
+      transaction.recentBlockhash = blockhash;
+      transaction.feePayer = publicKey;
+
+      const signature = await sendTransaction(transaction, connection);
+
+      console.log({ signature });
+
+      const confirmTransaction = await connection.confirmTransaction(
+        { signature, blockhash, lastValidBlockHeight },
+        "confirmed"
+      );
+
+      console.log({ confirmTransaction });
+
+      toast.success(
+        `Transaction successful with signature: ${formatString(
+          signature,
+          5,
+          5
+        )}`
+      );
+
+      setIsLoading(false);
+      getStakedTokenBalance(publicKey);
+      fetchUserBalance(publicKey);
+    } catch (error) {
+      toast.error("Something went wrong. Please try again");
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (publicKey) {
+      getStakedTokenBalance(publicKey);
+      fetchUserBalance(publicKey);
+    }
+  }, [publicKey]);
   return (
     <div className="flex flex-col mb-8 bg-[#1E1E1E] font-roboto  text-white ">
       <div className="flex mb-6">
         <button
           onClick={() => setActiveTab("buy")}
-          className={`mr-6 text-lg font-medium ${
+          className={`cursor-pointer mr-6 text-lg font-medium   ${
             activeTab === "buy" ? "text-[#A6E264]" : "text-gray-400"
           }`}
         >
@@ -91,7 +254,7 @@ export default function TradeToken() {
         </button>
         <button
           onClick={() => setActiveTab("sell")}
-          className={`text-lg font-medium ${
+          className={`cursor-pointer text-lg font-medium ${
             activeTab === "sell" ? "text-[#A6E264]" : "text-gray-400"
           }`}
         >
@@ -106,8 +269,8 @@ export default function TradeToken() {
           isInput={false}
           inputValue={`${Number(solAmount) * 2}`}
           imgStr={plntd_token}
-          tokenName="PLNTD"
-          tokenBalance="0"
+          tokenName="PLANTD"
+          tokenBalance={`${(plntdBalance / 1000000).toFixed(2)}`}
           tokenNameCheckout="You Buy"
         />
       )}
@@ -118,7 +281,7 @@ export default function TradeToken() {
           inputValue={solAmount}
           imgStr={sol_token}
           tokenName="SOL"
-          tokenBalance="3.2"
+          tokenBalance={`${solBalance.toFixed(2)}`}
           tokenNameCheckout="You Spend"
         />
       )}
@@ -126,23 +289,23 @@ export default function TradeToken() {
       {/* Selling PLNTD */}
       {activeTab === "sell" && (
         <TradeCard
-          setSolAmount={setSolAmount}
+          setSolAmount={() => {}}
           isInput={false}
-          inputValue={"0.5"}
+          inputValue={`${Number(plntdAmount) / 2}`}
           imgStr={sol_token}
           tokenName="SOL"
-          tokenBalance="3.2"
+          tokenBalance={`${solBalance.toFixed(2)}`}
           tokenNameCheckout="You Spend"
         />
       )}
       {activeTab === "sell" && (
         <TradeCard
-          setSolAmount={() => {}}
+          setSolAmount={setPlntdAmount}
           isInput={true}
-          inputValue={solAmount}
+          inputValue={plntdAmount}
           imgStr={plntd_token}
-          tokenName="PLNTD"
-          tokenBalance="0"
+          tokenName="PLANTD"
+          tokenBalance={`${(plntdBalance / 1000000).toFixed(2)}`}
           tokenNameCheckout="You Sell"
         />
       )}
@@ -160,9 +323,10 @@ export default function TradeToken() {
       {activeTab === "sell" && (
         <Button
           disabled={isLoading}
+          onClick={handleSell}
           className="w-full mt-4 py-6 cursor-pointer text-base font-medium rounded-full bg-[#e76e50] hover:bg-[#d25d47] text-black"
         >
-          Sell PLNTD
+          Sell PLANTD
         </Button>
       )}
     </div>
